2f0ea422ff75ece8aef74faeea2a6968c21eb27f
Savalet <savinien.petitjean@gmail.com>
2023-07-09 23:08:22 +0200

    Init

---
+++ src/main/java/fr/bedwars1058cloudnetaddon/Bedwars1058_CloudNet_Addon.java
@@ -0,0 +1,17 @@
+package fr.bedwars1058cloudnetaddon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bedwars1058_CloudNet_Addon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
